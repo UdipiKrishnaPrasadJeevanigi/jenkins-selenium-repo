5e135b4eb11fa8a6f55598326f6a5c7cfeba7a0b
Udipikrishna Prasad <judipikrishnaprasad@gmail,com>
2025-08-17 21:11:29 +0530

    Jenkins_Selenium_commitv1

---
+++ src/test/java/com/maven/seleniumtests/JenkinsHomePageTest.java
@@ -0,0 +1,39 @@
+package com.maven.seleniumtests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinsHomePageTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void test(){
+        driver.get("https://www.google.com");
+    }
+}
